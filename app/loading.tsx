c894bcab0ff0c3f80bfc90296940287a29b95ab1
Dan Groleau <ddgroleau@gmail.com>
2023-06-05 22:01:43 -0400

    Update loading styles

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,8 +3,8 @@ import NameLogo from "@/components/NameLogo";
 
 const Loading = () => {
     return (
-        <div className="h-full w-full gap-8 -pt-16 flex flex-col justify-center items-center">
-            <div className="[&>*]:relative">
+        <div className="h-full w-full gap-8 flex flex-col justify-center items-center">
+            <div className="[&>*]:relative -mt-16 ">
                 <NameLogo />
             </div>
             Loading...
